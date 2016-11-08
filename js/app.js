922bcafc4560fab72bfa4a602faceb618cbedef1
svaka1 <svakacharla@sapient.com>
2016-11-08 18:02:18 +0530

    basic setup

---
+++ js/app.js
@@ -0,0 +1,5 @@
+var myApp = angular.module('sathyaApp',[]);
+
+myApp.controller('appController',['$scope',function($scope){
+  $scope.message = "Welcome to Angular Registration-Login component.";
+}])
