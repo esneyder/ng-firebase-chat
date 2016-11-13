a2005a90eb6a3e05701d6a295f33fb8bb9e562c3
svaka1 <svakacharla@sapient.com>
2016-11-13 15:31:44 +0530

    google signin authentication enabled

--- build/js/controllers/dashboard.js
+++ build/js/controllers/dashboard.js
@@ -1,10 +1,19 @@
 myApp.controller('dashboardController',['$scope','$state',function($scope,$state){
   $scope.welcomeMsg = "Welcome to Register-Login Component";
-
+  const provider = new firebase.auth.GoogleAuthProvider();
   $scope.logout = function(){
-    firebase.auth().signOut();
+    firebase.auth().signOut()
+
+    .then(function() {
+       $state.go('login');
+       console.log('Signout Succesfull')
+    }, function(error) {
+       console.log('Signout Failed')
+    });
 
   }
+  
+
   firebase.auth().onAuthStateChanged(firebaseUser => {
     if(firebaseUser){
       console.log("Logged in");
--- build/js/controllers/login.js
+++ build/js/controllers/login.js
@@ -1,5 +1,6 @@
 myApp.controller('loginController',['$scope','$firebaseObject','$state',function($scope,$firebaseObject,$state){
   //$scope.message = "Welcome to Register-Login Component";
+  const provider = new firebase.auth.GoogleAuthProvider();
 
   $scope.login=function(){
     $scope.message = "Welcome"+ $scope.user.email;
@@ -22,6 +23,31 @@ myApp.controller('loginController',['$scope','$firebaseObject','$state',function
     });
 
   }
+
+  $scope.googleSignin = function() {
+   firebase.auth()
+
+   .signInWithPopup(provider).then(function(result) {
+      var token = result.credential.accessToken;
+      var user = result.user;
+      if(user.emailVerified){
+        $state.go('dashboard')
+      }
+      console.log(token)
+      console.log(user)
+   }).catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      //var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      //var credential = error.credential;
+      console.log(error.code)
+      console.log(error.message)
+   });
+}
+
+
   //Display user name in login html header
   /*const rootRef = firebase.database().ref().child('Object'); // referring to database object in firebase
   $scope.object = $firebaseObject(rootRef); // assigning to your scope object to view in html
